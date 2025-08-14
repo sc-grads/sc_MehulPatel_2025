a61c32b9e0605aa98ff0152c7ae305ce4dd7bfe2
Mehul Patel <mehul.patel@sambeconsulting.com>
2025-08-14 14:43:09 +0200

    Section 2 videos 51- 66

---
+++ C#/LogicApp.cs
@@ -0,0 +1,151 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+bool isRainy = false;
+bool hasUmbrealla = false;
+
+//Logical operators
+
+//if (isRainy && hasUmbrealla) 
+//{
+//    Console.WriteLine("I am protected against rain");
+//}
+
+//if (!isRainy || hasUmbrealla)
+//{
+//    Console.WriteLine("I am not getting wet");
+//}
+
+//if (isRainy & hasUmbrealla)
+//{
+//    Console.WriteLine("I am getting wet");
+//}
+
+//Console.WriteLine("Ay OK!");
+//Console.ReadKey();
+
+
+//relational operators
+
+//int num1 = 5;
+//int num2 = 6;
+
+//int age = 16;
+//bool isWithParents = false;
+
+//if (age >= 13 && isWithParents)
+//{
+//    Console.WriteLine("Go party in the club!");
+//}else if (age >6)
+//{
+//    Console.WriteLine("Go party in school!");
+//}
+//else
+//{
+//    Console.WriteLine("Go party in Kindergarten!");
+//}
+//Console.ReadKey();
+
+
+//Eqaulity operator
+
+//int num1 = 0;
+//int num2 = 0;
+//int age = 0;
+//string address = " ";
+
+//bool isEqual = num1 == num2;
+//bool isnotEqual = num1 != num2;
+
+//Console.WriteLine("Please enter a whole number");
+//if (num1 == int.Parse(Console.ReadLine())) //we want to see if the number the user inputs is equal to the computer
+//{
+//    Console.WriteLine("Numbers are equal!");
+
+//    Console.WriteLine("Please enter your age");
+//    age = int.Parse(Console.ReadLine());
+//    if(age >= 18)   //nested IF statements
+//    {
+//        Console.WriteLine("Please enter your address, so that we can send you the prize");
+//        address = Console.ReadLine();
+//    }
+//    else
+//    {
+//        Console.WriteLine("Sorry you can not get your prize due to your age");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Numbers are not equal");
+//}
+
+//Console.ReadKey(); 
+
+//challenge clubbing or no clubbing 
+
+//Console.WriteLine("How old are you? Write a whole number");
+//int age = int.Parse(Console.ReadLine());
+
+//if (age >= 18)
+//{
+//    Console.WriteLine("Go party in the club!");
+//}
+//else if (age >= 13)
+//{
+//    Console.WriteLine("Are you with your parents? Answer with y or n");
+//    string isWithParentsString = Console.ReadLine();
+
+//    if (isWithParentsString == "y")
+//    {
+//        Console.WriteLine("Go party in the club with your parents!");
+//    }
+//    else
+//    {
+//        Console.WriteLine("No party for you today");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Go party in Kindergarten!");
+//}
+//Console.ReadKey();
+
+
+//one line if statements
+//int month = 5;
+//string monthname;
+
+//if (month == 1)
+//{
+//    monthname = "January";
+//}
+//else if (month == 2)
+//    monthname = "February";
+//else if (month == 3)
+//    monthname = "March";
+//else
+//    monthname = "Unknown";
+
+//switch statement
+//switch (month)
+//{
+//    case 1:
+//        monthname = "January";
+//        break;
+
+//    case 2:
+//        monthname = "February";
+//        break;
+
+//    case 3:
+//        monthname = "March";
+//        break;
+
+//    default:
+//        monthname = "Unknown";
+//        break;
+
+
+//}
+
+//Console.WriteLine($"The month is { monthname}");
