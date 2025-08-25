ac63e0f53c4186d9798cd1bf793196a716ae6247
Mehul Patel <mehul.patel@sambeconsulting.com>
2025-08-25 16:57:07 +0200

    Section 5 quiz app

---
+++ C#/Section 5 Quizz App/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace QuizAppSect5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Question[] questions = new Question[]
+            {
+                new Question("What is the capital of Germany?", new string[] { "Paris", "Berlin", "London", "Mardid"}, 1),
+                new Question("What is 1 + 1?", new string[] {"2", "4", "5", "6"}, 0)
+            };
+
+            Quiz myQuiz = new Quiz(questions);
+            myQuiz.StartQuiz();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Section 5 Quizz App/Question.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizAppSect5
+{
+    internal class Question
+    {
+        public string QuestionText { get; set; }
+        public string[] Answer { get; set; }
+        public int CorrectAnswerIndex { get; set; }
+
+        public Question(string questionText, string[] answer, int correctAnswer)
+        {
+            QuestionText = questionText;
+            Answer = answer;
+            CorrectAnswerIndex = correctAnswer;
+        } 
+        
+        public bool IsCorrectAnswer(int choice)
+        {
+            return CorrectAnswerIndex == choice;
+        }
+
+        
+    }
+}
