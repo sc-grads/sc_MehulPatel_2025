42a015333ee252fe467115735d9bb9dc1a108623
Mehul Patel <mehul.patel@sambeconsulting.com>
2025-09-05 19:40:32 +0200

    Section 21

---
+++ C#/GenericTaskManagementSystem/GenericTaskManagementSystem/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTaskManagementSystem
+{
+    internal interface ITask<TResult>
+    {
+        TResult Perform();
+    }
+
+    class EmailTask : ITask<string>
+    {
+        public string Recipient { get; set; }
+        public string Message { get; set; }
+        public string Perform()
+        {
+            return $"Email sent to {Recipient} with message: {Message}";
+        }
+    }
+
+    class ReportTask : ITask<string>
+    {
+        public string ReportName { get; set; }
+        public string Perform()
+        {
+            return $"Report {ReportName} generated.";
+        }
+    }
+
+    class TaskProcessor<TTask, TResult> where TTask : ITask<TResult>
+    {
+        private TTask task;
+        public TaskProcessor(TTask taskParam)
+        {
+            task = taskParam;
+        }
+        public TResult Execute()
+        {
+            return task.Perform();
+        }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var emailTask = new EmailTask { Recipient = "example@example.com", Message = "Hello, this is a test email" };
+            var reportTask = new ReportTask { ReportName = "Annual Report" };
+            var emailProcessor = new TaskProcessor<EmailTask, string>(emailTask);
+            var reportProcessor = new TaskProcessor<ReportTask, string>(reportTask);
+
+            Console.WriteLine(emailProcessor.Execute());
+            Console.WriteLine(reportProcessor.Execute());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Generics/Generics/Box.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    // A generic class that can hold a value of any type.
+    internal class Box<T>
+    {
+        public T Content { get; set; }
+
+        public string Log()
+        {
+            return $"Box contains: {Content}";
+        }
+
+
+        //Video 357
+        private T content;
+
+        public Box(T content)
+        {
+            this.content = content;
+        }
+
+        public void UpdateContent(T newContent)
+        {
+            content = newContent;
+            Console.WriteLine($"Content updated to: {content}");
+        }
+
+        public T GetContent()
+        {
+            return content;
+        }
+
+    internal class Box<TFirst, TSecond>
+    {
+        public TFirst First { get; set; }
+        public TSecond Second { get; set; }
+
+        public Box(TFirst first, TSecond second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public void DisplayContents()
+        {
+            Console.WriteLine($"First: {First}, Second: {Second}");
+        }
+    }
+
+}
